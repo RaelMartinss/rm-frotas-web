8833d07b1000ce9d0cb1fdffb4a66c03e6c4310c
RaelMartinss <raelpgm2@gmail.com>
2026-09-10 16:58:32 -0300

    feat(android): add permanent release keystore for reproducible APK signing and bump to v1.0.8

--- src/app/core/services/app-update.service.ts
+++ src/app/core/services/app-update.service.ts
@@ -27,7 +27,7 @@ interface AppInstallerPlugin {
 
 const AppInstaller = registerPlugin<AppInstallerPlugin>('AppInstaller');
 
-export const CURRENT_NATIVE_VERSION = '1.0.7';
+export const CURRENT_NATIVE_VERSION = '1.0.8';
 const GITHUB_REPO = 'RaelMartinss/rm-frotas-web';
 
 @Injectable({
