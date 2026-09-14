b549afa90addae66d8b795cbab46b2e4aaad41dc
RaelMartinss <raelpgm2@gmail.com>
2026-09-14 17:44:27 -0300

    chore(config): configure Sentry DSN in frontend environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/v1',
-  sentryDsn: '',
+  sentryDsn: 'https://a20fd0d9bfc675be5221c7aaca7d4e9b@o4512086413606912.ingest.us.sentry.io/4512086569582592',
 };
